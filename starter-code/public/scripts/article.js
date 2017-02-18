0de86a336a6c5ee03ce471fe090a61c9197b5dfd
Brandon Little <brlittle86@gmail.com>
2017-02-17 20:29:15 -0800

    WE DID IT (collab with Christina and Alex)

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -83,12 +83,20 @@
 
   Article.numWordsByAuthor = () => {
     return Article.allAuthors().map(author => {
-      // TODO: Transform each author string into an object with properties for
+      // DONE: Transform each author string into an object with properties for
       // the author's name, as well as the total number of words across all articles
       // written by the specified author.
-      console.log(author);
-      if (Article.all.forEach())
-      
+      return {
+        name: author,
+        words: Article.all.filter(function (article) {
+          return article.author === author;
+        }).map(function (article) {
+          return article.body.split(' ').length
+        }).reduce(function (acc, current) {
+          return acc + current;
+        })
+      }
+    })
   };
 
   Article.truncateTable = callback => {
